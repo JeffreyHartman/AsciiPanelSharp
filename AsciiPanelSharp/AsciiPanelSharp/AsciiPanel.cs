ec1df1afcef3d72ad39d0b2608b69d908db2ad65
JeffreyHartman <fuego104@roadrunner.com>
2014-11-17 13:28:59 -0500

    Added Write and Clear methods.  Wrote the PaintScreen method to do the actual drawing of the terminal.

--- AsciiPanelSharp/AsciiPanelSharp/AsciiPanel.cs
+++ AsciiPanelSharp/AsciiPanelSharp/AsciiPanel.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics.Eventing;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
@@ -74,6 +76,9 @@ namespace AsciiPanelSharp
 
         public AsciiPanel(int width, int height)
         {
+            DoubleBuffered = true;
+            Paint += new PaintEventHandler(PaintScreen);
+
             if (width < 1 || height < 1)
             {
                 throw new ArgumentOutOfRangeException("width and height must be greater than 0");
@@ -93,7 +98,36 @@ namespace AsciiPanelSharp
             this.Clear();
 
             InitializeComponent();
+        }
 
+
+        private void PaintScreen(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            for (int x = 0; x < WidthInCharacters; x++)
+            {
+                for (int y = 0; y < HeightInCharacters; y++)
+                {
+                    Color bg = BackgroundColors[x, y];
+                    Color fg = BackgroundColors[x, y];
+
+                    var imageAttributes = new ImageAttributes();
+                    var backgroundColorMap = new ColorMap();
+                    var foregroundColorMap = new ColorMap();
+
+                    backgroundColorMap.OldColor = Color.FromArgb(255, 0, 0, 0);
+                    backgroundColorMap.NewColor = bg;
+
+                    foregroundColorMap.OldColor = Color.FromArgb(255, 168, 168, 168);
+                    foregroundColorMap.NewColor = fg;
+
+                    ColorMap[] remapTable = {backgroundColorMap, foregroundColorMap};
+
+                    imageAttributes.SetRemapTable(remapTable, ColorAdjustType.Bitmap);
+
+                    e.Graphics.DrawImage(Glyphs[Chars[x,y]], new Rectangle((x * CharWidth), (y * CharHeight), CharWidth, CharHeight), 0,0, CharWidth, CharHeight, GraphicsUnit.Pixel, imageAttributes);
+                }
+            }
         }
 
         private void LoadGlyphs()
@@ -123,7 +157,107 @@ namespace AsciiPanelSharp
 
         public AsciiPanel Clear()
         {
-            throw new NotImplementedException();
+            return Clear(' ', 0, 0, WidthInCharacters, HeightInCharacters, DefaultForegroundColor,
+                DefaultBackgroundColor);
+        }
+
+        public AsciiPanel Clear(char character)
+        {
+            return Clear(character, 0, 0, WidthInCharacters, HeightInCharacters, DefaultForegroundColor,
+                DefaultBackgroundColor);
+        }
+
+        public AsciiPanel Clear(char character, Color foreground, Color background)
+        {
+            return Clear(character, 0, 0, WidthInCharacters, HeightInCharacters, foreground, background);
+        }
+
+        public AsciiPanel Clear(char character, int x, int y, int width, int height)
+        {
+            return Clear(character, x, y, width, height, DefaultForegroundColor, DefaultBackgroundColor);
+        }
+
+        public AsciiPanel Clear(char character, int x, int y, int width, int height, Color foreground, Color background)
+        {
+            for (int xo = x; xo < x + width; xo++)
+            {
+                for (int yo = y; yo < y + height; yo++)
+                {
+                    Write(character, xo, yo, foreground, background);
+                }
+            }
+
+            return this;
+        }
+
+        public AsciiPanel Write(char character)
+        {
+            return Write(character, _cursorX, _cursorY, DefaultForegroundColor, DefaultBackgroundColor);
+        }
+
+        public AsciiPanel Write(char character, Color foreground)
+        {
+            return Write(character, _cursorX, _cursorY, foreground, DefaultBackgroundColor);
+        }
+
+        public AsciiPanel Write(char character, Color foreground, Color background)
+        {
+            return Write(character, _cursorX, _cursorY, foreground, background);
+        }
+
+        public AsciiPanel Write(char character, int x, int y)
+        {
+            return Write(character, x, y, DefaultForegroundColor, DefaultBackgroundColor);
+        }
+
+        public AsciiPanel Write(char character, int x, int y, Color foreground)
+        {
+            return Write(character, x, y, foreground, DefaultBackgroundColor);
+        }
+
+        public AsciiPanel Write(char character, int x, int y, Color foreground, Color background)
+        {
+            Chars[x, y] = character;
+            ForegroundColors[x, y] = foreground;
+            BackgroundColors[x, y] = background;
+            _cursorX = x + 1;
+            _cursorY = y;
+            return this;
+        }
+
+        public AsciiPanel Write(string str)
+        {
+            return Write(str, _cursorX, _cursorY, DefaultForegroundColor, DefaultBackgroundColor);
+        }
+
+        public AsciiPanel Write(string str, Color foreground)
+        {
+            return Write(str, _cursorX, _cursorY, foreground, DefaultBackgroundColor);
+        }
+
+        public AsciiPanel Write(string str, Color foreground, Color background)
+        {
+            return Write(str, _cursorX, _cursorY, foreground, background);
+        }
+
+        public AsciiPanel Write(string str, int x, int y)
+        {
+            return Write(str, x, y, DefaultForegroundColor, DefaultBackgroundColor);
+        }
+
+        public AsciiPanel Write(string str, int x, int y, Color foreground)
+        {
+            return Write(str, x, y, foreground, DefaultBackgroundColor);
+        }
+
+        public AsciiPanel Write(string str, int x, int y, Color foreground, Color background)
+        {
+            for (int i = 0; i < str.Length; i++)
+            {
+                Write(str[i], x + i, y, foreground, background);
+            }
+
+            return this;
         }
     }
 }
